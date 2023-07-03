21212289083ba61f15c0b01a7224879485bb0c08
thanhnguyen07 <thanhjang2k@gmail.com>
2023-07-03 17:35:56 +0700

    ⚙️ Config firebase login with google

---
+++ lib/features/authentication/application/login_with_google_controller.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginWithGoogleController extends StateNotifier {
+  LoginWithGoogleController(this.ref) : super(null);
+
+  final Ref ref;
+
+  void loginWithGoogle() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
--- lib/features/authentication/auth_providers.dart
+++ lib/features/authentication/auth_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jobs_pot/features/authentication/application/auth_controller.dart';
+import 'package:jobs_pot/features/authentication/application/login_with_google_controller.dart';
 import 'package:jobs_pot/features/authentication/application/onboarding_controller.dart';
 import 'package:jobs_pot/features/authentication/application/remember_logIn_controller.dart';
 import 'package:jobs_pot/features/authentication/application/sign_up_controller.dart';
@@ -24,6 +25,11 @@ final splashControllerProvider = StateNotifierProvider<SplashController, bool>(
 
 final loginControllerProvider = StateNotifierProvider<LoginController, dynamic>(
   (ref) => LoginController(ref),
+);
+
+final loginWithGoogleController =
+    StateNotifierProvider<LoginWithGoogleController, dynamic>(
+  (ref) => LoginWithGoogleController(ref),
 );
 
 final signUpControllerProvider =
