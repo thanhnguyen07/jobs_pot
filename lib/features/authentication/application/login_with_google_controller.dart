dd55339bf964b85621f83e93c4e3bd54a968477e
thanhnguyen07 <thanhjang2k@gmail.com>
2023-07-04 17:17:50 +0700

    🔐 Login with Google: Continue

--- lib/features/authentication/application/login_with_google_controller.dart
+++ lib/features/authentication/application/login_with_google_controller.dart
@@ -19,26 +19,16 @@ class LoginWithGoogleController extends StateNotifier {
   }
 
   Future<UserCredential> signInWithGoogle() async {
-    // GoogleSignIn _googleSignIn = GoogleSignIn(
-    //   scopes: [
-    //     'email',
-    //     'https://www.googleapis.com/auth/contacts.readonly',
-    //   ],
-    // );
-    // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
 
-    // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
 
-    // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
